e14f479ab2a3e957d2bf47880354461a0ff246dc
Itteba Gilani <itteba1@gmail.com>
2024-07-05 15:57:58 -0400

    update chat

--- components/ChatWindow.tsx
+++ components/ChatWindow.tsx
@@ -98,9 +98,6 @@ export function ChatWindow({
 
   async function sendMessage(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    if (messageContainerRef.current) {
-      messageContainerRef.current.classList.add("grow");
-    }
     if (chatEndpointIsLoading || intermediateStepsLoading) {
       return;
     }
@@ -181,13 +178,25 @@ export function ChatWindow({
     }
   }, [messages]);
 
+  const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    handleInputChange(e);
+    e.target.style.height = "auto"; // Reset height
+    e.target.style.height = `${e.target.scrollHeight}px`; // Set height to the scroll height
+  };
+
   return (
-    <div className="flex flex-col h-full p-4 md:p-2 bg-[#fcf0d5] rounded grow overflow-hidden border md:border-none">
-      <h2 className="text-2xl text-[#122f2d] font-black text-center py-2 hidden md:block">
-        {titleText}
+    <div
+      className="flex flex-col h-full p-4 md:p-2 bg-[#fcf0d5] rounded grow overflow-hidden"
+    >
+      <h2
+        className={`${
+          messages.length > 0 ? "" : "hidden"
+        } text-2xl text-[#122f2d] font-black text-center py-2`}
+      >
+        {emoji} {titleText}
       </h2>
       <div
-        className="flex flex-col w-full mb-4 overflow-auto transition-[flex-grow] ease-in-out grow"
+        className="flex flex-col w-full mb-4 overflow-auto flex-grow"
         ref={messageContainerRef}
       >
         {messages.length > 0 &&
@@ -210,37 +219,52 @@ export function ChatWindow({
       {messages.length === 0 && ingestForm}
 
       <form onSubmit={sendMessage} className="flex flex-col w-full mt-auto">
-        {intermediateStepsToggle}
-        <div className="flex w-full mt-4 space-x-2">
-          <input
-            className=" px-2 rounded text-white h-12"
+        <div className="flex">{intermediateStepsToggle}</div>
+        <div className="flex w-full mt-4">
+          <textarea
+            className="px-2 py-4 rounded text-white resize-none overflow-hidden h-12 flex-grow"
             value={input}
             placeholder={placeholder ?? "What services does Ittsy offer?"}
-            onChange={handleInputChange}
+            onChange={handleTextareaChange}
+            rows={1}
           />
           <button
             type="submit"
-            className="py-4 bg-[#122f2d] rounded w-full md:w-auto px-4 md:px-10 h-12 flex justify-center items-center"
+            className="shrink-0 py-4 bg-[#122f2d] rounded w-fit px-10"
           >
-            <div role="status" className={`${(chatEndpointIsLoading || intermediateStepsLoading) ? "" : "hidden"} flex justify-center w-fit px-2`}>
-              <svg aria-hidden="true" className="w-6 h-6 text-white animate-spin dark:text-white fill-sky-800" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+            <div
+              role="status"
+              className={`${
+                chatEndpointIsLoading || intermediateStepsLoading ? "" : "hidden"
+              } flex justify-center w-fit px-2`}
+            >
+              <svg
+                aria-hidden="true"
+                className="w-6 h-6 text-white animate-spin dark:text-white fill-sky-800"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5533C95.2932 28.8227 92.871 24.3691 89.8167 20.348C85.8452 15.1192 80.8826 10.7232 75.2124 7.55339C69.5422 4.38355 63.2754 2.5469 56.7649 2.13151C51.7644 1.77678 46.7593 2.05054 41.832 2.94208C39.3252 3.38837 37.861 5.91097 38.4981 8.33639C39.1352 10.7618 41.6077 12.1796 44.1299 11.7911C47.7725 11.2324 51.4886 11.0954 55.1346 11.3705C60.2629 11.7359 65.2306 13.278 69.6153 15.9057C73.9999 18.5334 77.7096 22.201 80.3767 26.6436C82.5893 30.1018 84.2413 33.8895 85.3028 37.889C86.0286 40.2471 88.5423 41.6781 90.9676 41.0409Z"
+                  fill="currentFill"
+                />
               </svg>
-              <span className="sr-only">Loading...</span>
             </div>
-            <span
-              className={
+            <div
+              className={`${
                 chatEndpointIsLoading || intermediateStepsLoading ? "hidden" : ""
-              }
+              }`}
             >
               Send
-            </span>
+            </div>
           </button>
         </div>
       </form>
-      <ToastContainer />
     </div>
   );
 }
-
