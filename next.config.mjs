2e43087f8ebc48e4593ae28be292cc07b714966b
Itteba Gilani <itteba1@gmail.com>
2024-07-07 17:26:49 -0400

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,51 @@
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      outputFileTracingExcludes: {
-        '**/*': [
-          './.next/cache/**',
-          './.next/trace/**',
-        ],
-      },
-      serverComponentsExternalPackages: [
-        'sharp',
-        'onnxruntime-node',
+  experimental: {
+    outputFileTracingExcludes: {
+      '**/*': [
+        './.next/cache/**',
+        './.next/trace/**',
       ],
     },
-    webpack: (config) => {
-      // Ignore node-specific modules when bundling for the browser
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        'sharp$': false,
-        'onnxruntime-node$': false,
-      };
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+    serverComponentsExternalPackages: [
+      'sharp',
+      'onnxruntime-node',
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    // Ignore node-specific modules when bundling for the browser
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'sharp$': false,
+      'onnxruntime-node$': false,
+    };
+
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+
+    if (!isServer) {
+      config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
+    }
+
+    config.plugins = [
+      ...config.plugins,
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "node_modules/tiktoken/lite/tiktoken_bg.wasm",
+            to: "static/wasm/tiktoken_bg.wasm",
+            toType: "file",
+          },
+        ],
+      }),
+    ];
+
+    return config;
+  },
+};
+
+export default nextConfig;
