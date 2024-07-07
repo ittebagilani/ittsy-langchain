6bc3b22eb3c4279d1ddccea16f484c62a3fc8e28
Itteba Gilani <itteba1@gmail.com>
2024-07-07 17:32:43 -0400

    remove tiktoken

--- next.config.mjs
+++ next.config.mjs
@@ -1,51 +1,27 @@
-import CopyWebpackPlugin from 'copy-webpack-plugin';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    outputFileTracingExcludes: {
-      '**/*': [
-        './.next/cache/**',
-        './.next/trace/**',
+    experimental: {
+      outputFileTracingExcludes: {
+        '**/*': [
+          './.next/cache/**',
+          './.next/trace/**',
+        ],
+      },
+      serverComponentsExternalPackages: [
+        'sharp',
+        'onnxruntime-node',
       ],
     },
-    serverComponentsExternalPackages: [
-      'sharp',
-      'onnxruntime-node',
-    ],
-  },
-  webpack: (config, { isServer }) => {
-    // Ignore node-specific modules when bundling for the browser
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'sharp$': false,
-      'onnxruntime-node$': false,
-    };
-
-    config.experiments = {
-      asyncWebAssembly: true,
-      layers: true,
-    };
-
-    if (!isServer) {
-      config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
-    }
-
-    config.plugins = [
-      ...config.plugins,
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: "node_modules/tiktoken/lite/tiktoken_bg.wasm",
-            to: "static/wasm/tiktoken_bg.wasm",
-            toType: "file",
-          },
-        ],
-      }),
-    ];
-
-    return config;
-  },
-};
-
-export default nextConfig;
+    webpack: (config) => {
+      // Ignore node-specific modules when bundling for the browser
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'sharp$': false,
+        'onnxruntime-node$': false,
+      };
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
