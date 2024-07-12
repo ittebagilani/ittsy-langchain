16ab0281e087bc54e629245fb8a60b0cd5fc61dc
Itteba Gilani <itteba1@gmail.com>
2024-07-12 17:26:24 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -42,9 +42,9 @@ const nextConfig = {
             // matching all API routes
             source: "/api/:path*",
             headers: [
-                { key: "Access-Control-Allow-Credentials", value: "false" },
+                { key: "Access-Control-Allow-Credentials", value: "true" },
                 { key: "Access-Control-Allow-Origin", value: "https://ittsy-llama-itteba-gs-projects.vercel.app/api/chat" }, // replace this your actual origin
-                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                { key: "Access-Control-Allow-Methods", value: "POST" },
                 { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
             ]
         }
